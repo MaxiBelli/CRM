c240e1a7aacd74a982753ae92eeac3afcbd9b031
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-08 17:22:07 -0300

    feat: Add Customer Form

--- src/pages/NewCustomer.jsx
+++ src/pages/NewCustomer.jsx
@@ -1,5 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import Form from "../components/Form";
+
 function NewCustomer() {
-  return <div>New Customer</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">New Customer</h1>
+      <p className="mt-3">Fill in all the fields to register a new customer</p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Form />
+
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Register Customer"
+          />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default NewCustomer;
